1096ebe2167fcc155dec637a47e30a7c2efa5817
fransay <francisannin5@gmail.com>
2022-06-11 14:23:28 +0000

    reverse factorial -fransay

---
+++ mathprobs/reversefactorial/fransay.js
@@ -0,0 +1,22 @@
+
+/*
+Given a positive integer, N, devise a computer solution that can be used to 
+find another integer M whose factorial is N.
+For example if N is given as 720 then M should be 6.
+*/ 
+
+
+function reversefactorial(n){
+  numstart = 1 
+  product = 1
+  while (true){
+    numstart++
+    product *=numstart
+    if (product==n){
+      break
+    }
+  }
+  return "M: "+ numstart
+}
+
+console.log(reversefactorial(720))
