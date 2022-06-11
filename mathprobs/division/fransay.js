6fd42fba17f23f9390cc2565e74afdcc19ede8a5
fransay <francisannin5@gmail.com>
2022-06-11 14:58:56 +0000

    update reversefactorial -fransay

---
+++ mathprobs/division/fransay.js
@@ -0,0 +1,29 @@
+/*
+Without using the division (/) devise a computer solution that can be used to m goes into n as well as the remainder. 
+The values for m and n will be read as inputs.
+*/ 
+
+
+function div(m,n){
+  // func returns divisible number + remainder without using the division sign
+  /* m is dividend and n is divisor 
+   since */
+  let constantcount = 1
+  let prod
+  while (true){
+    prod = constantcount *n 
+    
+    if (prod==m){
+      return m + " is divisible by "+n +" ,=>"+ constantcount + " times with no remainder"
+    }else if(prod >m){
+      let newconst = constantcount-1
+      return m + " is divisible by "+ n +" ,=>  "+ newconst + " times" + " with a remainder of "+ (m - (newconst)*n)
+
+    }
+    constantcount++
+  }
+}
+
+
+console.log(div(100,5))
+
---
+++ mathprobs/occurence/fransay.js
@@ -0,0 +1,15 @@
+/*Write down a computer solution that can be used to count the number of times a particular 
+digit appears in an (integer) input. Hint you may assume that there is an operator % that returns the 
+remainder when one integer is divided by another and has the 
+syntax a %b where a and b are integer constants. */
+
+
+function occur(digit){
+  numberoftime = 0
+
+  return numberoftimes
+
+}
+
+
+
